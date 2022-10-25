297918b017214d29e686e3c88d9d4b5e09d967a4
KianChew <112918780+KianChew@users.noreply.github.com>
2022-10-25 12:11:14 +0800

    moved typedef to header

--- Project1/Project1/menu.c
+++ Project1/Project1/menu.c
@@ -1,5 +1,7 @@
 #include "cprocessing.h"
 #include "menu.h"
+
+#pragma once
 #define TutorialWalls (10)
 
 
@@ -7,12 +9,6 @@ double title_alpha = 0;
 double tutorial_alpha = 0;
 int loop = 0;
 
-typedef struct tutorial_map {
-	float x;
-	float y;
-	float w;
-	float h;
-}map;
 
 
 void mainmenu_init(void) {
--- Project1/Project1/menu.h
+++ Project1/Project1/menu.h
@@ -5,3 +5,9 @@ void game_init(void);
 void game_update(void);
 void game_exit(void);
 
+typedef struct tutorial_map {
+	float x;
+	float y;
+	float w;
+	float h;
+}map;
